43b748617e9cc9ab2837255a6ae0548c38d3d6e8
johnlombardi389 <lombardij38@gmail.com>
2022-12-27 20:45:42 -0500

    Added cards to home for upcoming, popular, and new games

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -14,6 +14,33 @@ const GlobalStyles = createGlobalStyle`
       background-color: darkgray;
     }
   }
+  body {
+    width: 100%;
+  }
+  h2 {
+    font-family: 'Play', sans-serif;
+    font-size: 2rem;
+    font-weight: 400;
+    color: #333;
+  }
+  h3 {
+    font-family: 'Mulish', sans-serif;
+    font-size: 1.3rem;
+    font-weight: 700;
+    padding: 1.5rem;
+    color: #333;
+  }
+  p {
+    font-family: 'Mulish', sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 200%;
+    color: #696969;
+  }
+  a {
+    text-decoration: none;
+    color: #333;
+  }
 `;
 
 export default GlobalStyles;
