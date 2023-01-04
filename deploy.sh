b14ed89e6f086725fac42d3be7756ec7b525b305
johnlombardi389 <lombardij38@gmail.com>
2023-01-04 14:48:27 -0500

    Fixed github link in deploy

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git checkout -B main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:johnlombardi389/game-tracker.github.io.git main
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:johnlombardi389/game-tracker.git main:gh-pages
 
 cd -
